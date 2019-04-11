86d6c2973030e507b9d286a1fee4808a13ec96e6
Sapna Patil <sapnapatil1305@gmail.com>
2019-04-11 18:45:09 +0530

    Added employe class in model folder

--- WebAPI/Controllers/ValuesController.cs
+++ WebAPI/Controllers/ValuesController.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public List<Employee> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Employee> list = new List<Employee>() {
+                new Employee{EmpID=1,EmpName="Sapna",Job="Developer", Salary="30000",Address="Hyderabad" },
+                new Employee{EmpID=2,EmpName="Ranjeet",Job="Developer", Salary="30000",Address="Delhi" },
+                new Employee{EmpID=3,EmpName="Arti",Job="Developer", Salary="30000",Address="Hyderabad" },
+                new Employee{EmpID=4,EmpName="Manoj",Job="Tester", Salary="30000",Address="Pune" },
+                new Employee{EmpID=5,EmpName="Priya",Job="Accountant", Salary="450000",Address="Pune" },
+                new Employee{EmpID=6,EmpName="Albert",Job="Manager", Salary="850000",Address="Canada" }
+            };
+            return list;
         }
 
         // GET api/values/5
---
+++ WebAPI/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string Job { get; set; }
+        public string Salary { get; set; }
+        public string Address { get; set; }
+    }
+}
